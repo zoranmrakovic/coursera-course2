357462072c2c36a8b830026f51d27c4d071064d5
Zoran <zoran.mrakovic@gmail.com>
2016-12-15 11:19:25 +0100

    Update common.module.js

--- assignments/module5-solution/src/common/common.module.js
+++ assignments/module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://ychaikin-course5.herokuapp.com')
+.constant('ApiPath', 'https://rocky-wave-44003.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
